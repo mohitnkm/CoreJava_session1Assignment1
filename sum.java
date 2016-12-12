f82ae138ca980a3c9caa2e5f4d58bcab88a624fd
Mohit Nikam <mohitnkm@gmail.com>
2016-12-12 14:33:47 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,19 @@
+//Write a program to print the sum of two numbers without using + operator. Other operators have to be used.
+
+
+//Ans-
+
+
+
+public class Sum
+
+ {
+ 
+ public static void main(String[] args)
+ {
+int a = 10; 
+int b = 2;
+int sum = b-(-a);
+System.out.println(sum);
+}
+ }
